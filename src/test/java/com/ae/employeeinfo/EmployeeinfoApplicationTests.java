cba4d74059e670b70c9bf5d18f752a220157126d
GBTC440014UR <aevans7>
2020-03-09 19:30:45 -0400

    Initial commit

---
+++ src/test/java/com/ae/employeeinfo/EmployeeinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ae.employeeinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
